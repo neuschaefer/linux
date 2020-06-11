727dad75b1cd2d9558b152f779109dd4b267223b
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 21:04:33 +0200

    Apply PATCH6/39-drivers_net_arm_wpcm450_mac.h

--- drivers/net/arm/wpcm450_mac.h
+++ drivers/net/arm/wpcm450_mac.h
@@ -5,7 +5,7 @@
 #ifndef _WPCM450_MAC_H_
 #define _WPCM450_MAC_H_
 
-#if defined(CFG_PROJ_NETWORK_ETH0) || defined(CFG_PROJ_NETWORK_ETH1)
+#if defined(CFG_PROJ_NETWORK_ETH0) || defined(CFG_PROJ_NETWORK_ETH1) || defined(CFG_PROJ_NETWORK_BOOTSELECT)
 #define MAX_WPCM_MAC  	 1
 #elif defined(CFG_PROJ_NETWORK_BOTH)
 #define MAX_WPCM_MAC  	 2
@@ -224,12 +224,14 @@ typedef struct
 #define MGSTA_SQE		(1<<10)
 #define MGSTA_TXHA		(1<<11)
 
+extern int isMACInitialized;
+unsigned long init_wpcm_mac_eth(void);
 
 #define wpcm_WriteReg(reg,val,which) \
-  (*((volatile unsigned int *)(wpcm_mac_io[(which)]+(reg)))=(val))
+(!isMACInitialized && init_wpcm_mac_eth())?(*((volatile unsigned int *)(wpcm_mac_io[(which)]+(reg)))=(val)):(*((volatile unsigned int *)(wpcm_mac_io[(which)]+(reg)))=(val))
 
 #define wpcm_ReadReg(reg,which)      \
- (*((volatile unsigned int *)(wpcm_mac_io[(which)]+(reg))))
+  (!isMACInitialized && init_wpcm_mac_eth())?(*((volatile unsigned int *)(wpcm_mac_io[(which)]+(reg)))): (*((volatile unsigned int *)(wpcm_mac_io[(which)]+(reg))))
 
 #define wpcm_WriteCam0(which,x,lsw,msw) \
 		wpcm_WriteReg(CAM0L+(x)*CAM_ENTRY_SIZE,lsw,which);\
