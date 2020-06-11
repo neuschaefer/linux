358a6093008be3860d03e93905980794f739b609
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 21:04:27 +0200

    Apply PATCH1/21-net_ipv4_devinet.c

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -103,6 +103,41 @@ static void devinet_sysctl_register(struct in_device *in_dev,
 static void devinet_sysctl_unregister(struct ipv4_devconf *p);
 #endif
 
+/* AMI Extension Begin */
+void (*ipv4_config_fn) (unsigned long) = NULL;
+char ipv4_config_if[32];
+
+int
+RegisterIPV4Config(char *name,void (*fn)(unsigned long))
+{
+	if (ipv4_config_fn != NULL)
+		return 1;
+	ipv4_config_fn = fn;
+	strcpy(ipv4_config_if,name);
+	return 0;
+}
+
+int
+UnRegisterIPV4Config(char *name)
+{
+	if (strcmp(name,ipv4_config_if) != 0)
+	{
+		printk("WARNING: UnRegisterIPV4Config : Removing unregistered function\n");
+		ipv4_config_fn = NULL;
+		return 1;
+	}	
+	ipv4_config_fn = NULL;
+	return 0;
+}
+
+
+
+EXPORT_SYMBOL(RegisterIPV4Config);
+EXPORT_SYMBOL(UnRegisterIPV4Config);
+
+/* AMI Extension End */
+
+
 /* Locks all the inet devices. */
 
 static struct in_ifaddr *inet_alloc_ifa(void)
@@ -756,6 +791,14 @@ int devinet_ioctl(unsigned int cmd, void __user *arg)
 		}
 
 		ifa->ifa_address = ifa->ifa_local = sin->sin_addr.s_addr;
+
+/* AMI Extension Begin */
+		if (ipv4_config_fn)
+		{
+			if (strcmp(ipv4_config_if,ifr.ifr_name) == 0)
+				ipv4_config_fn(sin->sin_addr.s_addr);
+		}
+/* AMI Extension End */
 
 		if (!(dev->flags & IFF_POINTOPOINT)) {
 			ifa->ifa_prefixlen = inet_abc_len(ifa->ifa_address);
