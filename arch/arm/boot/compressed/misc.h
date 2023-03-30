138a579547e6bd48819811fc6b1d738c77da7121
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2023-03-30 04:55:15 +0200

    ARM: compressed: Pass the actual output length to the decompressor
    ZSTD writes outside of the space that is necessary for the uncompressed
    data, when it is told it has unlimited output length. To fix this, pass
    the actual output length (the length of the uncompressed kernel) to the
    decompressor.
    
    The uncompressed length is already stored as a little endian 32-bit
    constant before the input_data_end symbol. This has been the case since
    commit bc22c17e12c13 ("bzip2/lzma: library support for gzip, bzip2 and
    lzma decompression"), which has been merged in 2.6.30.
    
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Tony Lindgren <tony@atomide.com>
    ---
    
    v2:
    - Document when the appended size appeared
    - Add R-b tags

--- arch/arm/boot/compressed/misc.h
+++ arch/arm/boot/compressed/misc.h
@@ -13,7 +13,7 @@ decompress_kernel(unsigned long output_start, unsigned long free_mem_ptr_p,
 void fortify_panic(const char *name);
 int atags_to_fdt(void *atag_list, void *fdt, int total_space);
 uint32_t fdt_check_mem_start(uint32_t mem_start, const void *fdt);
-int do_decompress(u8 *input, int len, u8 *output, void (*error)(char *x));
+int do_decompress(u8 *input, int len, u8 *output, int outlen, void (*error)(char *x));
 
 extern char input_data[];
 extern char input_data_end[];
