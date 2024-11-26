19d9702b305fc89c5e3b073ae2efc8e1ce7f0b2f
J. Neuschäfer <j.ne@posteo.net>
2024-11-26 15:01:48 +0100

    powerpc: Generalize MPC831x platform support
    The Reference Design Boards (RDB) don't have the same relevance they had
    then the MPC831x platform was new; if any work is done today, then
    likely based on used production boards, which are more readily available
    than NXP's discontinued devboards.
    
    To further reduce the focus on RDBs, add DT compatible strings for all
    four MPC8314/5 variants.
    
    Signed-off-by: J. Neuschäfer <j.ne@posteo.net>

--- arch/powerpc/platforms/83xx/mpc831x.c
+++ arch/powerpc/platforms/83xx/mpc831x.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * arch/powerpc/platforms/83xx/mpc831x_rdb.c
+ * arch/powerpc/platforms/83xx/mpc831x.c
  *
- * Description: MPC831x RDB board specific routines.
+ * Description: MPC831x board specific routines.
  * This file is based on mpc834x_sys.c
  * Author: Lo Wlison <r43300@freescale.com>
  *
@@ -22,24 +22,31 @@
 /*
  * Setup the architecture
  */
-static void __init mpc831x_rdb_setup_arch(void)
+static void __init mpc831x_setup_arch(void)
 {
 	mpc83xx_setup_arch();
 	mpc831x_usb_cfg();
 }
 
 static const char *board[] __initdata = {
+	/* Reference design boards */
 	"MPC8313ERDB",
 	"fsl,mpc8315erdb",
+
+	/* Generic chip compatibles */
+	"fsl,mpc8315e",
+	"fsl,mpc8315",
+	"fsl,mpc8314e",
+	"fsl,mpc8314",
 	NULL
 };
 
-machine_device_initcall(mpc831x_rdb, mpc83xx_declare_of_platform_devices);
+machine_device_initcall(mpc831x, mpc83xx_declare_of_platform_devices);
 
-define_machine(mpc831x_rdb) {
-	.name			= "MPC831x RDB",
+define_machine(mpc831x) {
+	.name			= "MPC831x",
 	.compatibles		= board,
-	.setup_arch		= mpc831x_rdb_setup_arch,
+	.setup_arch		= mpc831x_setup_arch,
 	.discover_phbs		= mpc83xx_setup_pci,
 	.init_IRQ		= mpc83xx_ipic_init_IRQ,
 	.get_irq		= ipic_get_irq,
