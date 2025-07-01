e6b40d1781d206924ada8fb03e914206f175c880
J. Neuschäfer <j.ne@posteo.net>
2025-07-01 22:55:45 +0200

    wip

--- arch/arm64/include/asm/hwcap.h
+++ arch/arm64/include/asm/hwcap.h
@@ -176,6 +176,7 @@
 #define KERNEL_HWCAP_POE		__khwcap2_feature(POE)
 
 #define __khwcap3_feature(x)		(const_ilog2(HWCAP3_ ## x) + 128)
+#define KERNEL_HWCAP_BIGEND		__khwcap3_feature(BIGEND)
 
 /*
  * This yields a mask that user programs can use to figure out what
